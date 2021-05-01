2fe488f4e004b58489896744a34031151931397b
Pattharathon Srithundorn <lemon.pattharathon@bitkub.com>
2021-05-02 00:31:19 +0700

    [EDIT] Remove unused route and information & change meta tag description.

--- apps/block_scout_web/config/config.exs
+++ apps/block_scout_web/config/config.exs
@@ -68,10 +68,10 @@ config :block_scout_web, BlockScoutWeb.Tracer,
 config :block_scout_web, BlockScoutWeb.Gettext, locales: ~w(en), default_locale: "en"
 
 config :block_scout_web, BlockScoutWeb.SocialMedia,
-  twitter: "PoaNetwork",
-  telegram: "poa_network",
-  facebook: "PoaNetwork",
-  instagram: "PoaNetwork"
+  twitter: "BitkubChain",
+  telegram: "bitkub_chain",
+  facebook: "BitkubChain",
+  instagram: "BitkubChain"
 
 # Configures History
 price_chart_config =
--- setup_env_variables.sh
+++ setup_env_variables.sh
@@ -1,4 +1,4 @@
-export LOGO=/images/logo.png
+export LOGO=/images/bitkubchain-logo.png
 export ETHEREUM_JSONRPC_VARIANT=geth
 export ETHEREUM_JSONRPC_HTTP_URL=http://47.241.3.150:8545
 export ETHEREUM_JSONRPC_WS_URL=ws://47.241.3.150:8546
