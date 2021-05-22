d07a944f04a3ed4b310ef4c2d62aa4b72e6f1332
Pattharathon Srithundorn <lemon.pattharathon@bitkub.com>
2021-05-22 18:34:46 +0700

    Add official label & change token standard name from ERC to KAP.

---
+++ env_prod.sh
@@ -0,0 +1,8 @@
+export LOGO=/images/bitkubchain-logo.png
+export ETHEREUM_JSONRPC_VARIANT=geth
+export ETHEREUM_JSONRPC_HTTP_URL=https://rpc.bitkubchain.io
+export ETHEREUM_JSONRPC_WS_URL=wss://wss.bitkubchain.io
+export ETHEREUM_JSONRPC_TRACE_URL=https://rpc.bitkubchain.io
+export NETWORK=Testnet
+export SUBNETWORK=BitkubChain
+export BLOCK_TRANSFORMER=clique
