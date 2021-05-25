4c22937bcc8137f086c7112a4dbfa4f668e616b3
KPowen <owen.kamin@bitkub.com>
2021-05-26 00:42:39 +0800

    Update

---
+++ docker/envSetup/env_mainnet.sh
@@ -0,0 +1,11 @@
+export LOGO=/images/bitkubchain-logo.png
+export ETHEREUM_JSONRPC_VARIANT=geth
+export ETHEREUM_JSONRPC_HTTP_URL=http://110.238.115.30:8545
+export ETHEREUM_JSONRPC_WS_URL=ws://110.238.115.30:8546
+export ETHEREUM_JSONRPC_TRACE_URL=http://110.238.115.30:8545
+export BLOCKSCOUT_HOST=bkcscan.com
+export BLOCKSCOUT_PROTOCOL=https
+export NETWORK="Mainnet"
+export SUBNETWORK="Bitkub Chain"
+export BLOCK_TRANSFORMER=clique
+export DISABLE_EXCHANGE_RATES=true
