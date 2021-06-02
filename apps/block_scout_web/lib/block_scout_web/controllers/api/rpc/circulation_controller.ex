d30b1023bfac4b0a46017c7d7f87302d769a22b7
bam.kontee <>
2021-06-02 19:35:12 +0700

    fix bug get circulation API

--- apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/circulation_controller.ex
+++ apps/block_scout_web/lib/block_scout_web/controllers/api/rpc/circulation_controller.ex
@@ -1,10 +1,9 @@
 defmodule BlockScoutWeb.API.RPC.Circulation do
   use BlockScoutWeb, :controller
-
-  import BlockScoutWeb.Chain, only: [paging_options: 1, next_page_params: 3, split_list_by_page: 1]
-
   import Ecto.Query
-  alias Explorer.Chain
+
+  alias Explorer.{Repo}
+
 
   alias Explorer.Chain.{
     Address,
@@ -12,22 +11,37 @@ defmodule BlockScoutWeb.API.RPC.Circulation do
   }
 
 
-  defp get_circulation() do
-    query =
-      from( addr in Address,
-      select: fragment("1000000000 - SUM(addr.fetched_coin_balance_block_number)"),
-      where: [ fragment("addr.hash IN (
-        decode('D78A91F21B12de8793BB8616961F31BEefa97fE4','hex'),
-        decode('FE28FfD8c528066bE3aE4B0D95db0478EfBA7413','hex'),
-        decode('755FD36eC8F6E2eb69bCeC441bacA89855747d50','hex'),
-        decode('c9760469d7635916D20329e4bac99ffBBFD38DF4','hex'),
-        decode('6DfBA1364e9627b863c6188a6545000e675E5898','hex'),
-        decode('c6422b55db8b3288a1f8316bc07375948e08082f','hex'),
-        decode('52cB901D945B7499f1457d36b882a59cDe9664D5','hex')
-      )")
-    ]
+  def get_circulation(conn, _params) do
+    # address_list = ["decode('D78A91F21B12de8793BB8616961F31BEefa97fE4','hex')","decode('FE28FfD8c528066bE3aE4B0D95db0478EfBA7413','hex')","decode('755FD36eC8F6E2eb69bCeC441bacA89855747d50','hex')","decode('c9760469d7635916D20329e4bac99ffBBFD38DF4','hex')","decode('6DfBA1364e9627b863c6188a6545000e675E5898','hex')","decode('c6422b55db8b3288a1f8316bc07375948e08082f','hex')","decode('52cB901D945B7499f1457d36b882a59cDe9664D5','hex')"]
+    query = from( a0 in Address,
+        select: fragment("SUM(a0.fetched_coin_balance_block_number)"),
+        where: fragment(" a0.hash in (
+          decode('D78A91F21B12de8793BB8616961F31BEefa97fE4','hex'),
+          decode('FE28FfD8c528066bE3aE4B0D95db0478EfBA7413','hex'),
+          decode('755FD36eC8F6E2eb69bCeC441bacA89855747d50','hex'),
+          decode('c9760469d7635916D20329e4bac99ffBBFD38DF4','hex'),
+          decode('6DfBA1364e9627b863c6188a6545000e675E5898','hex'),
+          decode('c6422b55db8b3288a1f8316bc07375948e08082f','hex'),
+          decode('52cB901D945B7499f1457d36b882a59cDe9664D5','hex')
+        ) ")
       )
 
-    render(conn, Repo.one!(query, timeout: :infinity) || 0, %{status: true})
+    result_circulation = Repo.one!(query, timeout: :infinity) || 0
+
+    render(conn, "get_circulation.json", result: result_circulation)
+
+  end
+
+  def get_sumgas(conn, _params) do
+
+    query =
+      from(
+        t0 in Transaction,
+        select: fragment("SUM(t0.gas_used)")
+      )
+
+    result_sum_gas = Repo.one!(query, timeout: :infinity) || 0
+
+    render(conn, "get_sumgas.json", result: result_sum_gas)
   end
 end
