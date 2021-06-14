47c36dc495f9012f1e1d75d1c2611292b65e1f4c
Pattharathon srithundorn <lemon.pattharathon@bitkub.com>
2021-06-14 15:36:13 +0700

    Update env_mainnet.sh

--- env_mainnet.sh
+++ env_mainnet.sh
@@ -10,3 +10,6 @@ export SUBNETWORK="Bitkub Chain"
 export BLOCK_TRANSFORMER=clique
 export DISABLE_EXCHANGE_RATES=true
 export ENABLE_GOOGLE_ANALYTICS=true
+export ENABLE_TXS_STATS=true
+export SHOW_PRICE_CHART=true
+export SHOW_TXS_CHART=true
