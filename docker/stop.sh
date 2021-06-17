5921766a1a79204d35a7b8fe30dbddd4720cf554
KPowen <owen.kamin@bitkub.com>
2021-06-17 05:17:45 +0000

    [ADD] additional docker container cleaning step

--- docker/stop.sh
+++ docker/stop.sh
@@ -1,2 +1,4 @@
-sudo docker stop bkc-explorer
-sudo docker rm bkc-explorer
+CONTAINER=$(docker ps | grep bkc-explorer | awk '{print $1}')
+sudo docker stop $CONTAINER
+wait 1
+sudo docker rm $CONTAINER
