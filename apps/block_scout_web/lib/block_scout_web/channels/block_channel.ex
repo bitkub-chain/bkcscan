94e6bd84c8cebe9ad9bef3c1e99e14cb1481b28a
bam.kontee <>
2021-06-23 13:05:34 +0700

    fix fetch current lang

--- apps/block_scout_web/lib/block_scout_web/channels/block_channel.ex
+++ apps/block_scout_web/lib/block_scout_web/channels/block_channel.ex
@@ -20,7 +20,6 @@ defmodule BlockScoutWeb.BlockChannel do
   def handle_out("new_block", %{block: block, average_block_time: average_block_time}, socket) do
     # Gettext.put_locale(BlockScoutWeb.Gettext, socket.assigns.locale)
     Gettext.put_locale(BlockScoutWeb.Gettext, Gettext.get_locale(BlockScoutWeb.Gettext) )
-    
     rendered_block =
       View.render_to_string(
         BlockView,
