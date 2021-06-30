ada725dbeee36c0254c3bced547c21356edc72fc
bam-kontee <84561307+bam-kontee@users.noreply.github.com>
2021-06-30 14:11:56 +0700

    Update config.exs
    fix coin-gecko-id

--- apps/explorer/config/config.exs
+++ apps/explorer/config/config.exs
@@ -8,10 +8,10 @@ use Mix.Config
 # General application configuration
 config :explorer,
   ecto_repos: [Explorer.Repo],
-  coin: System.get_env("COIN") || "KUB",
-  # coin: "KUB",
-  coingecko_coin_id: System.get_env("COINGECKO_COIN_ID") || "bitkub-coin",
-  # coingecko_coin_id: "bitkub-coin",
+  #coin: System.get_env("COIN") || "KUB",
+  coin: "KUB",
+  #coingecko_coin_id: System.get_env("COINGECKO_COIN_ID") || "bitkub-coin",
+  coingecko_coin_id: "bitkub-coin",
   token_functions_reader_max_retries: 3,
   allowed_evm_versions:
     System.get_env("ALLOWED_EVM_VERSIONS") ||
