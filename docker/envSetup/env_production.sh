adf31c543af6499054ebb089903f3a6667e1b1ba
Pattharathon srithundorn <lemon.pattharathon@bitkub.com>
2021-07-16 17:31:45 +0700

    Create env_production.sh

---
+++ docker/envSetup/env_production.sh
@@ -0,0 +1,17 @@
+export LOGO=/images/bitkubchain-logo.png
+export ETHEREUM_JSONRPC_VARIANT=geth
+export ETHEREUM_JSONRPC_HTTP_URL=http://10.0.0.99:8545
+export ETHEREUM_JSONRPC_WS_URL=ws://10.0.0.99:8546
+export ETHEREUM_JSONRPC_TRACE_URL=http://10.0.0.99:8545
+export BLOCKSCOUT_HOST=bkcscan.com
+export BLOCKSCOUT_PROTOCOL=https
+export NETWORK="Mainnet"
+export SUBNETWORK="Bitkub Chain"
+export BLOCK_TRANSFORMER=clique
+export DISABLE_EXCHANGE_RATES=false
+export ENABLE_GOOGLE_ANALYTICS=true
+export ENABLE_TXS_STATS=false
+export SHOW_PRICE_CHART=true
+export SHOW_TXS_CHART=false
+export COIN="KUB"
+export COINGECKO_COIN_ID="bitkub-coin"
