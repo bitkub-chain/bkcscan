099ac1439e4b1b2550a7557150763ac0882f1ad3
Pattharathon srithundorn <lemon.pattharathon@bitkub.com>
2021-07-16 17:04:13 +0700

    Update env_mainnet.sh

--- docker/envSetup/env_mainnet.sh
+++ docker/envSetup/env_mainnet.sh
@@ -1,11 +1,11 @@
 export LOGO=/images/bitkubchain-logo.png
 export ETHEREUM_JSONRPC_VARIANT=geth
-export ETHEREUM_JSONRPC_HTTP_URL=http://110.238.115.30:8545
-export ETHEREUM_JSONRPC_WS_URL=ws://110.238.115.30:8546
-export ETHEREUM_JSONRPC_TRACE_URL=http://110.238.115.30:8545
-export BLOCKSCOUT_HOST=testnet.bkcscan.com
+export ETHEREUM_JSONRPC_HTTP_URL=http://10.0.2.16:8545
+export ETHEREUM_JSONRPC_WS_URL=ws://10.0.2.16:8546
+export ETHEREUM_JSONRPC_TRACE_URL=http://10.0.2.16:8545
+export BLOCKSCOUT_HOST=bkcscan.com
 export BLOCKSCOUT_PROTOCOL=https
-export NETWORK="Testnet"
+export NETWORK="Mainnet"
 export SUBNETWORK="Bitkub Chain"
 export BLOCK_TRANSFORMER=clique
 export DISABLE_EXCHANGE_RATES=false
