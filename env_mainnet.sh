72d8ba66f340646cfa8663e541e3ba98c49d1192
root <lemon.pattharathon@bitkub.com>
2021-08-17 18:46:53 +0800

    updates

--- env_mainnet.sh
+++ env_mainnet.sh
@@ -1,18 +1,17 @@
 export LOGO=/images/bitkubchain-logo.png
 export ETHEREUM_JSONRPC_VARIANT=geth
-export ETHEREUM_JSONRPC_HTTP_URL=http://159.138.245.69:8545
-export ETHEREUM_JSONRPC_WS_URL=ws://159.138.245.69:8546
-export ETHEREUM_JSONRPC_TRACE_URL=http://159.138.245.69:8545
+export ETHEREUM_JSONRPC_HTTP_URL=http://10.0.2.16:8545
+export ETHEREUM_JSONRPC_WS_URL=ws://10.0.2.16:8546
+export ETHEREUM_JSONRPC_TRACE_URL=http://10.0.2.16:8545
 export BLOCKSCOUT_HOST=bkcscan.com
 export BLOCKSCOUT_PROTOCOL=https
 export NETWORK="Mainnet"
 export SUBNETWORK="Bitkub Chain"
 export BLOCK_TRANSFORMER=clique
-export DISABLE_EXCHANGE_RATES=true
-export ENABLE_TXS_STATS=true
+export DISABLE_EXCHANGE_RATES=false
+export ENABLE_GOOGLE_ANALYTICS=false
+export ENABLE_TXS_STATS=false
 export SHOW_PRICE_CHART=true
-export SHOW_TXS_CHART=true
+export SHOW_TXS_CHART=false
 export COIN="KUB"
 export COINGECKO_COIN_ID="bitkub-coin"
-export ENABLE_GOOGLE_ANALYTICS=true
-
